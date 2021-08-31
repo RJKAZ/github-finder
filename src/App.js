3da637b0e8459c08e3ea06851accc5d5ec43a659
RJ <rjkaz@comcast.net>
2021-08-30 23:42:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,51 +6,15 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios';
+
 
 import GithubState from './context/github/GithubState';
 
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Get Single Github User
-
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  // Get Users Repos
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-
-  // clear users from state
-
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
 
   // Set Alert
   const showAlert = (msg, type) => {
@@ -72,29 +36,15 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      clearUsers={clearUsers}
-                      showClear={users.length > 0 ? true : false}
                       showAlert={showAlert}
                     />
-                    <Users loading={loading} users={users} />
+                    <Users />
                   </Fragment>
                 )}
               />
               <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
-              />
+              <Route exact path='/user/:login' component={User}/>
+              
             </Switch>
           </div>
         </div>
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -16,6 +16,25 @@ import {
                 users: action.payload,
                 loading: false 
             };
+        case GET_USER:
+            return {
+                ...state, 
+                user: action.payload,
+                loading: false
+            }
+        case CLEAR_USERS:
+            return {
+                ...state,
+                users: [],
+                loading: false 
+            }
+        case GET_REPOS: {
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
+            }
+        }
         case SET_LOADING:
             return { 
                 ...state,
